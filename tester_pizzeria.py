eba73db76faa1b8282d0eb7b9cf777ae2a797de5
Diego Gomez <diegocor4@gmail.com>
2024-09-11 12:21:08 -0300

    tester en proceso

--- tester_pizzeria.py
+++ tester_pizzeria.py
@@ -6,4 +6,7 @@ class TesterPizzeria:
     def main(self):
         # Punto 5.a. Crear objetos de tipo MaestroPizzero, Mozo y Pizza.
         maestro = MaestroPizzero('Don Pipo') # Un único objeto de tipo MaestroPizzero
+        mozo1 = Mozo('Alfredo') # Primer mozo
+        mozo2 = Mozo('Carlos') # Segundo mozo
         
+        # Punto 5.b. Enviar mensajes a MaestroPizzero: tomarPedido, cocinar y entregar
